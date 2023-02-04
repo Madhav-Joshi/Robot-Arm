7c29e917734302045b1fff43918c6eb9d8bc67ef
Madhav-Joshi <madhav16701@gmail.com>
2023-02-04 19:32:08 +0530

    Created a final fully organised folder for final control block

--- Final_torque/functions_data/torque6dof.m
+++ Final_torque/functions_data/torque6dof.m
@@ -3,53 +3,19 @@ function torque = torque6dof(q,q_dot,q_dotdot)
     % required q, q_dot and q_dotdot
     %% Define 
     n = 6;
-    global DH l11 l12 l21 l22 l3 l41 l42 l5 l61 l62;
+    load robot_description.mat inertia DH;
     dh = DH(q);
     
-    % Center of mass
-    lc11 = l11/2; lc12 = l12/2;
-    lc21 = l21/2; lc22 = l22/2;
-    lc3 = l3/2;
-    lc41 = l41/2; lc42 = l42/2;
-    lc5 = l5/2;
-    lc61 = l61/2; lc62 = l62/2;
-
     lc = zeros(3,n+1); % COM coordinates in zero position wrt frame {0}
-    lc(:,1) = [0;lc12;lc11]; % COM1 coord in frame 0
-    lc(:,2) = [lc22;l12;l11-lc21]; % COM2 coord in frame 1
-    lc(:,3) = [l22;l12;l11-l21-lc3];
-    lc(:,4) = [l22-lc41;l12;l11-l21-l3-lc42];
-    lc(:,5) = [l22-l41+lc5;l12;l11-l21-l3-l42];
-    lc(:,6) = [l22-l41+l5+lc61;l12;l11-l21-l3-l42+lc62];
-    lc = [lc;ones(1,n+1)]; % For multiplication with transformation matrices
-    
-    T0 = zeros(4,4,n); % Transformation matix from frame 0 to frame i
-    for i=1:n
-        T = eye(4);
-        for j=1:i
-            T = T*transDH(dh(j,1:4));
-        end
-        T0(:,:,i) = T; % Store transformation matrices from 0 to i
-    end
-    
     m = zeros(n); % mass of links
-    for i=1:n
-        m(i) = 1;
-    end
-    
     I0 = zeros(3,3,n); % Inertia of links on COM in ground frame orientation
-    for i=1:n
-        I0(:,:,i) = eye(3);
+    for i=1:6
+        lc(:,i) = inertia(i,3:5)';
+        m(i) = inertia(i,2);
+        I0(:,:,i) = reshape(inertia(i,6:end),[3 3])';
     end
-
-%     r = 2.5; 
-%     I{1} = zeros(3,3);
-%     I{2} = [m{2}*l2^2/12+m{2}*r^2/4 0 0; 0 m{2}*r^2/2 0; 0 0 m{2}*l2^2/12+m{2}*r^2/4];
-%     I{3} = [m{3}*l3^2/12+m{3}*r^2/4 0 0; 0 m{3}*l3^2/12+m{3}*r^2/4 0; 0 0 m{3}*r^2/2];
-%     I{4} = [m{4}*l4^2/12+m{4}*r^2/4 0 0; 0 m{4}*l4^2/12+m{4}*r^2/4 0; 0 0 m{4}*r^2/2];
-%     I{5} = [m{5}*l5^2/12+m{5}*r^2/4 0 0; 0 m{5}*l5^2/12+m{5}*r^2/4 0; 0 0 m{5}*r^2/2];
-%     I{6} = [m{6}*l6^2/12+m{6}*r^2/4 0 0; 0 m{6}*l6^2/12+m{6}*r^2/4 0; 0 0 m{6}*r^2/2];
-
+    lc = [lc;ones(1,n+1)]; % For multiplication with transformation matrices
+       
     V0 = [0;0;0;0;0;0]; % Twist of the frame 0
     Vdot0 = [0;0;0;0;0;-9.81];
     
@@ -58,6 +24,16 @@ function torque = torque6dof(q,q_dot,q_dotdot)
     F(:,7) = Ftip; % Defining F_{n+1} = F7 = Ftip
 
     %% Don't do anything below
+
+    T0 = zeros(4,4,n); % Transformation matix from frame 0 to frame i
+    for i=1:n
+        T = eye(4);
+        for j=1:i
+            T = T*transDH(dh(j,1:4));
+        end
+        T0(:,:,i) = T; % Store transformation matrices from 0 to i
+    end
+
      % xc is the COMi coordinates wrt dh frame i-1 
     xc = zeros(4,n+1);
     xc(:,1) = lc(:,1);
@@ -72,12 +48,6 @@ function torque = torque6dof(q,q_dot,q_dotdot)
 
     % Screw axis of joint i in frame i
     A = zeros(6,n);
-%     A(:,1) = [0;0;0;0;0;1];
-%     A(:,2) = [0;0;1;-lc2;0;0];
-%     A(:,3) = [0;0;1;0;0;0];
-%     A(:,4) = [0;0;1;lc42;0;0];
-%     A(:,5) = [0;0;1;-lc5;0;0];
-%     A(:,6) = [0;0;1;lc62;0;0];
     
     for i=1:n
         A(:,i) = [0;0;dh(i,5);(-dh(i,5)*box3([0;0;1])*xc(1:3,i) + (1-dh(i,5))*[0;0;1])];
