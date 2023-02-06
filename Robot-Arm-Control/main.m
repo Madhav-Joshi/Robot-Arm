2ce725ccfba858fd293c19a1a5a6bee54b376c40
tejalbarnwal <tejalbarnwal@gmail.com>
2023-02-06 16:53:23 +0530

    r

--- Robot-Arm-Control/main.m
+++ Robot-Arm-Control/main.m
@@ -68,17 +68,4 @@ b_dim(:,6)=[109;84;97]/1000;
 
 save('robot_description.mat','n_links','f_c','b_dim','-append')
 
-%% visualize initial collision boxes
-Link_Frame=eye(4);
-collision_boxes{n_links}=[];
-for i=1:n_links
-    collision_boxes{i}=collisionBox(b_dim(1,i),b_dim(2,i),b_dim(3,i));
-    Link_Frame=Link_Frame*A(:,:,i);
-    collision_boxes{i}.Pose=Link_Frame*[eye(3),f_c(:,i);0 0 0 1]; 
-end
-for j=1:n_links        
-    show(collision_boxes{j})
-    hold on
-end
-hold off
-pause(0.1)
+
