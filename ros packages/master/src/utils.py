87b468ec02136f9f38f8537555bd6eba1b6fb288
Shubham <shubhi.g2001@gmail.com>
2023-02-07 20:34:33 +0530

    correcte dfwd kinematics

--- ros packages/master/src/utils.py
+++ ros packages/master/src/utils.py
@@ -5,24 +5,21 @@ def transDH(dh):
     # or representing a vector in frame i-1 from frame i
     # input dh is a vector of [theta,d,a,alpha] in radians
     
-    T = ([[np.cos(dh[0]),  -np.sin(dh[0])*np.cos(dh[3]),  np.sin(dh[0])*np.sin(dh[3]),   dh[3]*np.cos(dh[0])],
-            [np.sin(dh[0]),  np.cos(dh[0])*np.cos(dh[3]),   -np.cos(dh[0])*np.sin(dh[3]),  dh[3]*np.sin(dh[0])],
-            [0,           np.sin(dh[3]),              np.cos(dh[3]),              dh[2]           ],
+    T = ([[np.cos(dh[0]),  -np.sin(dh[0])*np.cos(dh[3]),  np.sin(dh[0])*np.sin(dh[3]),   dh[2]*np.cos(dh[0])],
+            [np.sin(dh[0]),  np.cos(dh[0])*np.cos(dh[3]),   -np.cos(dh[0])*np.sin(dh[3]),  dh[2]*np.sin(dh[0])],
+            [0,           np.sin(dh[3]),              np.cos(dh[3]),              dh[1]           ],
             [0,           0,                      0,                       1]])
     return T
 
 def forwardKinematicsAllJoints(q):
     # This function takes in input as the joint angles and gives a (4,4,6)
     # array of joint transformations from frame zero to frame i (i=1:6)
-    l11 = 648.9/1000
-    l12 = 267.95/1000
-    l21 = 82/1000
-    l22 = 365.5/1000
-    l3 = 111.25/1000
-    l4 = 309/1000
-    l5 = 42.25/1000
-    l61 = 99/1000
-    l62 = 14.35/1000
+    l11 = 648.9/1000; l12 = 267.95/1000; 
+    l21 = 82/1000; l22 = 290/1000; 
+    l3 = 111.25/1000;  
+    l4 = 360/1000;
+    l5 = 42.25/1000;
+    l61 = 99/1000; l62 = 14.35/1000;
 
     DH = lambda q : np.array([  [np.pi/2,        l11+q[0,0],    l12,     0,       0],
             [-np.pi/2+q[1,0],  -l21,        l22,     0,       1],
@@ -32,10 +29,15 @@ def forwardKinematicsAllJoints(q):
             [-np.pi/2+q[5,0],  l5+l61,      l62,     0,       1]   ])
     
     dh = DH(q)
+    print(np.round(dh,2))
 
     T = np.eye(4)
     for j in range(6):
-        T[:,:] = T[:,:]*transDH(dh[j,:4])
+        T = T @ transDH(dh[j,:4])
+        #print(np.round(transDH(dh[j,:4]),2))
+        #print(np.round(T,2))
+        print('\n')
+
     return T
 
 def points_to_trans(w,x,y,z,qi):
@@ -62,8 +64,10 @@ def points_to_trans(w,x,y,z,qi):
     T6_d[:3,2:3] = normal
     print(T6_d)
     T06 = forwardKinematicsAllJoints(qi)
-    T = T06*T6_d # multiply T66_ in middle
+    T = T06 @ T6_d # multiply T66_ in middle
     return T
 
 if __name__=='__main__':
-    points_to_trans(np.array([0,0,0]),np.array([1,0,0]),np.array([1,-1,0]),np.array([0,-1,0]),np.array([[0,0,0,0,0,0]]).T)
+    # points_to_trans(np.array([0,0,0]),np.array([1,0,0]),np.array([1,-1,0]),np.array([0,-1,0]),np.array([[0,0,0,0,0,0]]).T)
+    
+    print(np.round(forwardKinematicsAllJoints(np.zeros([6,1])),2))
